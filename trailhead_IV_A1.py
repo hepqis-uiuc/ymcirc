607e1bfa818c4e7c96b06309d9f245fe9a3d965d
Andrew Lytle <atlytle@gmail.com>
2024-09-24 11:59:48 -0500

    Two plaquette Trotter evolution

--- trailhead_IV_A1.py
+++ trailhead_IV_A1.py
@@ -73,6 +73,14 @@ def test_H_decomposition(gsq):
     #print((H1 + H2 + H3) - H(gsq))
     print(np.allclose(H1 + H2 + H3, H(gsq), rtol=1e-16))
 
+def Tstep1_circ(th_II, th_IZ, th_ZI, th_IX, th_XI, dt):
+    circ = QuantumCircuit(2,2, global_phase=-th_II*dt)
+    circ.rz(2*th_IZ*dt, 0)
+    circ.rz(2*th_ZI*dt, 1)
+    circ.rx(2*th_IX*dt, 0)
+    circ.rx(2*th_XI*dt, 1)
+    return circ
+
 def Tstep3_circ(th_ZX, th_XZ, dt):
     "Eq. 43 of trailhead."
     circ = QuantumCircuit(2, 2)
@@ -84,18 +92,74 @@ def Tstep3_circ(th_ZX, th_XZ, dt):
     circ.h(1)
     circ.cx(1, 0)
     circ.h(1)
-
     return circ
 
 def test_Trotter_circuits():
     H1, H2, H3 = H1_H2_H3(1)
     dt = 0.2
-    circ3 = Tstep3_circ(-1/3, 1/(6*np.sqrt(2)), dt)
+    sqrt2 = np.sqrt(2)
+    circ1 = Tstep1_circ(7/3+23/8, 1/8-1, -(1/8+1), -2/3, -1/(6*sqrt2), dt)
+    eHtest = expm(-1j*(dt)*H1)
+    print(Operator.from_circuit(circ1))
+    print(Operator(eHtest))
+    print(Operator.from_circuit(circ1) == Operator(eHtest))
+    circ2 = Tstep2_circ(-1/(18*sqrt2), -1/(18*sqrt2), 1/8-1/3, dt)
+    eHtest = expm(-1j*(dt)*H2)
+    print(Operator.from_circuit(circ2) == Operator(eHtest))
+    circ3 = Tstep3_circ(-1/3, 1/(6*sqrt2), dt)
     eHtest = expm(-1j*(dt)*H3)
-    print(Operator.from_circuit(circ3))
-    print(eHtest)
     print(Operator.from_circuit(circ3) == Operator(eHtest))
+
+def Trotter_exact():
+    H1, H2, H3 = H1_H2_H3(1)
+    dts = np.linspace(0, 5, 100)
+    _res = [np.dot(expm(-1j*dt*H3), np.dot(expm(-1j*dt*H2), expm(-1j*dt*H1))) for dt in dts]
+    probs = [np.abs(_r[0,0])**2 for _r in _res]
+    return probs
+
+def run_Trotter(tmin, tmax, tsteps, order, trotter_steps):
+    dts = np.linspace(tmin, tmax, tsteps)
+    probs = []
+    time = Parameter("time")
+    sqrt2 = np.sqrt(2)
+    for _dt in dts:
+        sampler = Sampler()
+        Trotter_steps = [Tstep3_circ(-1/3, 1/(6*sqrt2), time), 
+                         Tstep2_circ(-1/(18*sqrt2), -1/(18*sqrt2), 1/8-1/3, time), 
+                         Tstep1_circ(7/3+23/8, 1/8-1, -(1/8+1), -2/3, -1/(6*sqrt2), time)]
+        circuit = Trotter_evol2([], Trotter_steps, time, _dt, order=order, nsteps=trotter_steps)
+        circuit.measure_all()
+        try:
+            probs.append(sampler.run(circuit).result().quasi_dists[0][0])
+        except KeyError:
+            probs.append(0)
+    return dts, probs
+
+def plot_state_evol():
+    dts = np.linspace(0, 5, 100)
+    probs = [np.abs(expm(-1j*dt*H(1, sector='++'))[0,0])**2 for dt in dts]
+    probs2 = Trotter_exact()
+
+    dts_trotter11, probs_trotter11 = run_Trotter(0, 2, 40, 1, 1)
+    dts_trotter14, probs_trotter14 = run_Trotter(0, 3, 40, 1, 4)
+    print(probs_trotter11)
+
+    fig, ax = plt.subplots()
+    ax.set_ylim([0.0, 1.1])
+    ax.set_xlim([0, 3])
+    ax.set_ylabel("P(0)")
+    ax.set_xlabel("time")
+    ax.plot(dts, probs, 'k-', label='exact')
+    ax.plot(dts, probs2, 'b-', label='Trotter1')
+    ax.plot(dts_trotter11, probs_trotter11, 'o--', ms=2, label='1 step')
+    ax.plot(dts_trotter14, probs_trotter14, 'o--', ms=2, label='4 steps')
+
+    plt.legend()
+    #plt.show()
+    plt.savefig("./trailhead_Fig12a.pdf")
+
 
 if __name__ == "__main__":
     test_H_decomposition(1)
     test_Trotter_circuits()
+    plot_state_evol()
