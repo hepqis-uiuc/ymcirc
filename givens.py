5d304c5272a0cc5599d5ba1a1667a004f2070f84
Jason K. Elhaderi <jasonke2@illinois.edu>
2024-11-04 13:27:34 -0600

    WIP stitching logic for givens rotations using dummy Hamiltonian.

---
+++ givens.py
@@ -0,0 +1,98 @@
+# Cianan Conefrey-Shinozaki
+# Oct 2024
+
+from qiskit import QuantumCircuit, QuantumRegister, AncillaRegister
+from qiskit.circuit.library import YGate, XGate
+from qiskit.circuit.library.standard_gates import RYGate, RXGate
+from qiskit.circuit import Parameter
+from qiskit.quantum_info import Operator
+
+
+def givens(bit_string_1: str,
+           bit_string_2: str,
+           angle: float,
+           reverse: bool = False) -> QuantumCircuit:
+    """
+    Perform a Givens rotation between two bitstrings.
+
+    Expects two bitstrings in physicist / big-endian notation.
+    e.g. "1001" or "0100", and a float as an angle. Optional reverse arg for little-endian.
+    """
+    if bit_string_1 == bit_string_2:  # returns identity
+        return QuantumCircuit(len(bit_string_1))
+
+    bit_string_1_reversed = bit_string_1[::-1]  # rewrite the string as little-endian Qiskit notation
+    bit_string_2_reversed = bit_string_2[::-1]
+
+    if len(bit_string_1) != len(bit_string_2):
+        raise ValueError('bitstrings must be the same length')
+
+    num_qubits = len(bit_string_1)
+
+    circ = QuantumCircuit(num_qubits)
+
+    if num_qubits == 1:
+        circ.rx(angle, 0)
+        return circ
+
+    for ii in range(num_qubits - 1, -1, -1):  # This finds where to put RY
+        if bit_string_1_reversed[ii] != bit_string_2_reversed[ii]:
+            target = ii
+            break
+
+    # This tells the later controls what value to control on
+    ctrl_val = 0 if bit_string_1_reversed[target] == "1" else 1
+
+    ctrls = list(range(0, num_qubits))
+    ctrls.remove(target)  # This provides the list of RX controls
+
+    ctrl_state = bit_string_1_reversed[:target] + bit_string_1_reversed[
+        (target + 1):]  # This provides what type of control
+    ctrl_state = ctrl_state[::-1]  # This reversal cancels some other reversal
+
+    multiRX = RXGate(angle).control(
+        num_ctrl_qubits=len(ctrls),
+        ctrl_state=ctrl_state)  # This builds the multicontrol X gate function
+
+    # obsolete:
+    # multiX = XGate().control(num_ctrl_qubits = 1, ctrl_state=ctrl_val)
+
+    Xcirc = QuantumCircuit(
+        num_qubits
+    )  # These are the "preamble" and "tail" control-X of the Givens
+
+    for ii in range(target - 1, -1,
+                    -1):  # These instruct where to put the X gates
+        if bit_string_1_reversed[ii] != bit_string_2_reversed[ii]:
+            Xcirc.cx(control_qubit=target,
+                     target_qubit=ii,
+                     ctrl_state=ctrl_val)
+
+    circ.append(multiRX, ctrls + [target])  # This adds multiY to the circuit
+
+    #obsolete:
+    #circ = Xcirc.compose(circ.compose(Xcirc))
+    circ.compose(Xcirc,
+                 inplace=True)  # inplace arg looks weird but is much faster!
+    Xcirc.compose(circ, inplace=True)
+    if reverse == True:
+        Xcirc = Xcirc.reverse_bits()
+    return Xcirc
+
+    #return circ
+    #Xcirc.draw()
+
+
+def test_givens():
+    test_circ = QuantumCircuit(2)
+    test_circ.cx(control_qubit=1, target_qubit=0, ctrl_state="1")
+    test_circ.append(RXGate(1).control(ctrl_state="1"), [0, 1])
+    test_circ.cx(control_qubit=1, target_qubit=0, ctrl_state="1")
+
+    op_test = Operator(test_circ)
+    op_given = givens('01', '10', 1)
+    assert op_test.equiv(op_given), "Failed first test"
+
+
+if __name__ == "__main__":
+    test_givens()
--- scratch.py
+++ scratch.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
-from conventions import VERTEX_SINGLET_DICT_D_3HALVES_1_3_3BAR_8
+from conventions import VERTEX_SINGLET_BITMAPS, IRREP_TRUNCATION_DICT_1_3_3BAR_6_6BAR_8
 from lattice_tools import LatticeRegisters, Plaquette, LatticeVector, LinkUnitVectorLabel
-from math import ceil
+from math import ceil, pi
+from givens import givens
 from itertools import product
 from qiskit.circuit import QuantumCircuit, QuantumRegister
 from typing import List, Tuple, Dict
@@ -14,9 +15,9 @@ def make_plaquette_circuit(
         primed_qubit_idx: int,
         prefix_target_qubit_idxes: list[int],
         rotation_ctrl_idxes: list[int],
-        rotation_ctrl_state: str,
+        rotation_ctrl_bitstring: str,
         rotation_angle: float) -> QuantumCircuit:
-    if len(rotation_ctrl_idxes) != len(rotation_ctrl_state):
+    if len(rotation_ctrl_idxes) != len(rotation_ctrl_bitstring):
         raise ValueError()
     # TODO replace this dummy circuit with actual simulation circuit
     n_qubits = sum([len(register) for register in plaquette.vertex_registers]) + \
@@ -31,56 +32,98 @@ def make_plaquette_circuit(
 
 
 if __name__ == "__main__":
-    #print(VERTEX_SINGLET_DICT_D_3HALVES_133BAR8)
-    for singlet in VERTEX_SINGLET_DICT_D_3HALVES_1_3_3BAR_8:
-        print(f"Singlet: {singlet} -> {VERTEX_SINGLET_DICT_D_3HALVES_1_3_3BAR_8[singlet]}")
-    lattice = LatticeRegisters(dim=2, size=3, n_qubits_per_vertex=2, n_qubits_per_link=2)
+    # Create lattice with encodings.
+    lattice = LatticeRegisters(
+        dim=1.5,
+        size=3,
+        link_truncation_dict=IRREP_TRUNCATION_DICT_1_3_3BAR_6_6BAR_8,
+        vertex_singlet_dict=VERTEX_SINGLET_BITMAPS["d=3/2, T2"]
+    )
+
+    # Log resulting lattice data.
     print(f"Created dim {lattice.dim} lattice with vertices:\n{lattice.vertex_register_keys}.")
+    print(f"It has {lattice.n_qubits_per_link} qubits per link and {lattice.n_qubits_per_vertex}.")
+    print("It knows about the following encodings:")
+    for irrep, encoding in lattice.link_truncation_bitmap.items():
+        print(f"Link irrep encoding: {irrep} -> {encoding}")
+    for vertex_bag, encoding in lattice.vertex_singlet_bitmap.items():
+        print(f"Vertex singlet bag encoding: {vertex_bag} -> {encoding}")
 
-    #plaquette_local_circuit = QuantumCircuit()
+    # Loop over plaquettes in the lattice to construct one trotter step
+    # TODO clean up code below for this.
+    # TODO decide whether some of this functionality should be moved to LatticeRegisters or another class.
+    plaquette_local_circuit = QuantumCircuit()
 
-    # master_circuit = QuantumCircuit(
-    #     *[lattice.get_link_register(key[0], key[1]) for key in lattice.link_register_keys],
-    #     *[lattice.get_vertex_register(key) for key in lattice.vertex_register_keys]
-    # )
-    # for vertex_address in lattice.vertex_register_keys:
-    #     if lattice.dim == 1.5 and vertex_address[1] == 1:
-    #         continue
-    #     current_vertex_reg = lattice.get_vertex_register(vertex_address)
-    #     print(f"Building plaquette at vertex {vertex_address}:", current_vertex_reg)
-    #     plaquettes = [lattice.get_plaquette_registers(vertex_address, 1, 2)]#[get_plaquette(lattice, vertex_address, 1, 2)]
-    #     for plaquette in plaquettes:
-    #         # TODO: delete, just for debug.
-    #         for link in plaquette.link_registers:
-    #             print(link)
-    #         for vertex in plaquette.vertex_registers:
-    #             print(vertex)
-    #         #local_hamiltonian_evol = QuantumCircuit()
-    #         local_hamiltonian_evol = make_plaquette_circuit(plaquette.vertex_registers, plaquette.link_registers)
-    #         print(local_hamiltonian_evol.draw())
-    #         vertex_qubits = []
-    #         link_qubits = []
-    #         for register in plaquette.vertex_registers:
-    #             for qubit in register:
-    #                 vertex_qubits.append(qubit)
+    # Assemble all lattice registers into a blank circuit
+    master_circuit = QuantumCircuit(
+        *[lattice.get_link_register(link_address[0], link_address[1]) for link_address in lattice.link_register_keys],
+        *[lattice.get_vertex_register(vertex_address) for vertex_address in lattice.vertex_register_keys]
+    )
 
-    #         for register in plaquette.link_registers:
-    #             for qubit in register:
-    #                 link_qubits.append(qubit)
-            
-    #         master_circuit = master_circuit.compose(local_hamiltonian_evol, qubits=[
-    #             *vertex_qubits,
-    #             *link_qubits
-    #         ])
+    # TODO consider interleaving application of electric and magnetic Hamiltonian evolution?
 
-    # print(master_circuit.draw())
+    # Loop over links for electric Hamiltonain
+    for link_key in lattice.link_register_keys:
+        # TODO Implement
+        pass
 
+    # Loop over plaquettes for magnetic Hamiltonian.
+    # TODO swap this out for real matrix elements. Format:
+    # (state bitstring1, state bitstring2, rotation angle)
+    import random
+    random.seed(0)
+    n_qubits_per_plaquette = 4 * (lattice.n_qubits_per_link + lattice.n_qubits_per_vertex)
+    n_matrix_elements = 2
+    TEST_DUMMY_MAG_HAMILTONIAN_DATA = [
+        (
+            ''.join(random.choice('01') for _ in range(n_qubits_per_plaquette)),
+            ''.join(random.choice('01') for _ in range(n_qubits_per_plaquette)),
+            random.uniform(0, 2 * pi)
+        )
+        for _ in range(n_matrix_elements)
+    ]
+    for vertex_address in lattice.vertex_register_keys:
+        has_no_vertical_periodic_link_three_halves_case = lattice.dim == 1.5 and vertex_address[1] == 1
+        if has_no_vertical_periodic_link_three_halves_case:
+            continue
 
-        # for link_dir in range(1, ceil(lattice.dim) + 1):
-        #     if lattice.dim == 1.5 and vertex_address[1] == 1 and link_dir == 2:
-        #         continue
+        current_vertex_reg = lattice.get_vertex_register(vertex_address)
+        print(f"Fetching all positive plaquettes at {vertex_address}:", current_vertex_reg)
+        has_only_one_positive_plaquette = lattice.dim == 1.5 or lattice.dim == 2
+        if has_only_one_positive_plaquette:
+            plaquettes: List[Plaquette] = [lattice.get_plaquette_registers(vertex_address, 1, 2)]
+        else:
+            plaquettes: List[Plaquette] = lattice.get_plaquette_registers(vertex_address)
+        print(f"Found {len(plaquettes)} plaquette(s).")
+        for plaquette in plaquettes:
+            # TODO: delete, just for debug.
+            for link in plaquette.link_registers:
+                print(link)
+            for vertex in plaquette.vertex_registers:
+                print(vertex)
 
-        #     link_address = (vertex_address, link_dir)
-        #     current_link_reg = lattice.get_link_register(vertex_address, link_dir)
-        #     print(f"Link {link_address}:", current_link_reg)
+            # Collect the local qubits for later stitching.
+            vertex_qubits = []
+            link_qubits = []
+            for register in plaquette.vertex_registers:
+                for qubit in register:
+                    vertex_qubits.append(qubit)
+            for register in plaquette.link_registers:
+                for qubit in register:
+                    link_qubits.append(qubit)
+            #local_hamiltonian_evol = QuantumCircuit()
+            # Append a givens rotation circuit for each matrix element
+            for bitstring_1, bitstring_2, angle in TEST_DUMMY_MAG_HAMILTONIAN_DATA:
+                # Get the current givens rotation circuit
+                # TODO make it so these are precomputed one time rather than inside nested for loops.
+                # Much more efficient to just do it once for all matrix elements, then create copies as needed.
+                plaquette_local_rotation_circuit = givens(bitstring_1, bitstring_2, angle)
+                #print(plaquette_local_rotation_circuit)
 
+                # Stitch into master circuit.
+                master_circuit = master_circuit.compose(plaquette_local_rotation_circuit, qubits=[
+                    *vertex_qubits,
+                    *link_qubits
+                ])
+
+    master_circuit.draw(output="mpl", filename="quantum_circuit.pdf", fold=False)
