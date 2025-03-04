c6845c666603b0b68cdcee1a819a27a8821604f1
pbalaji98 <36338571+pbalaji98@users.noreply.github.com>
2025-03-03 21:17:01 -0600

    Electric energy computation when vertex registers present (#34)
    * new dev branch for review; finds average electric energy for all systems
    
    * tweaked casimirs functions to rely on lattice_encoder
    
    * adding unphysical state handling and renaming dim var
    
    * made reversal False in plaquette_local_rotation_circuit in circuit.py, which fixed local endianess issue; fixed endianess variable naming in n_trotter_steps
    
    * Removed comment that's no longer accurate.
    
    ---------
    
    Co-authored-by: Jason K. Elhaderi <jasonke2@illinois.edu>

--- ymcirc/circuit.py
+++ ymcirc/circuit.py
@@ -225,13 +225,11 @@ class LatticeCircuitManager:
                     else:
                         physical_control_qubits = None
                     angle = -matrix_elem * (1 / (2 * (coupling_g**2))) * dt
-                    # We use the reverse argument to account for the little-endianness
-                    # of QuantumRegisters implemented by qiskit.
                     plaquette_local_rotation_circuit = givens(
                         bit_string_1,
                         bit_string_2,
                         angle,
-                        reverse=True,
+                        reverse=False,
                         physical_control_qubits=physical_control_qubits)
                     if optimize_circuits is True:
                         plaquette_local_rotation_circuit = transpile(
--- ymcirc/electric_helper.py
+++ ymcirc/electric_helper.py
@@ -6,20 +6,22 @@ construct the Pauli decomposition for matrix with 1 at (i,i) and weights
 decomposition by the electric Casimir (according to the link_bitmap).
 """
 from __future__ import annotations
-from ymcirc.conventions import (
-    IrrepWeight, BitString, IrrepBitmap, VertexMultiplicityBitmap
-)
+from ymcirc.conventions import (IrrepWeight, BitString, IrrepBitmap,
+                                LatticeStateEncoder)
 import numpy as np
 from typing import List
+import warnings
 
 
-def _bitwise_addition(
-        bit_string_1: str | BitString, bit_string_2: str | BitString) -> int:
+def _bitwise_addition(bit_string_1: str | BitString,
+                      bit_string_2: str | BitString) -> int:
     """Add two bit bit strings bitwise mod 2."""
-    return sum([(int(bit_string_1[i]) * int(bit_string_2[i])) for i in range(len(bit_string_1))]) % 2
+    return sum([(int(bit_string_1[i]) * int(bit_string_2[i]))
+                for i in range(len(bit_string_1))]) % 2
 
 
-def _diagonal_pauli_string(bit_string: str, N: int, casimir: float) -> List[int]:
+def _diagonal_pauli_string(bit_string: str, N: int,
+                           casimir: float) -> List[int]:
     """
     Generate a Pauli decomposition.
 
@@ -33,8 +35,8 @@ def _diagonal_pauli_string(bit_string: str, N: int, casimir: float) -> List[int]
     pauliz_list = []
     for i in range(2**N):
         i_string = str('{0:0' + str(N) + 'b}').format(i)
-        pauliz_list.append(
-            (1 / (2**N)) * casimir * ((-1)**(_bitwise_addition(i_string, bit_string))))
+        pauliz_list.append((1 / (2**N)) * casimir *
+                           ((-1)**(_bitwise_addition(i_string, bit_string))))
     return pauliz_list
 
 
@@ -49,48 +51,137 @@ def _gt_pattern_iweight_to_casimir(gt_tuple: IrrepWeight) -> float:
     return (p**2 + q**2 + p * q + 3 * p + 3 * q) / 3.0
 
 
+def _handle_electric_energy_unphysical_states(global_state: str,
+                                              link_state: str,
+                                              note_unphysical_states: bool,
+                                              stop_on_unphysical_states: bool) -> float:
+    """ 
+    Handle unphysical link states in electric energy calculation.
+    """
+    if (stop_on_unphysical_states):
+        # Terminate simulation
+        raise ValueError(
+            "Computation of electric energy terminated due to unphysical link state "
+            + str(link_state) + " in " + str(global_state))
+    elif (note_unphysical_states):
+        # Just print an error, but continue simulation with electric energy for the state 0.0
+        warning_msg = f"Unphysical link state {link_state} in {global_state} found during computation of electric energy."
+        warnings.warn(warning_msg)
+        return 0.0
+    else:
+        return 0.0
+
+
 def casimirs(link_bitmap: IrrepBitmap) -> List[float]:
     """Generate a list of Casimir eigenvalues from link bitmap i-Weights."""
-    return [_gt_pattern_iweight_to_casimir(irrep) for irrep in list(link_bitmap.keys())]
+    return [
+        _gt_pattern_iweight_to_casimir(irrep)
+        for irrep in list(link_bitmap.keys())
+    ]
 
 
-# TODO, implement this calculation for vertex_bitmap \neq {}.
 def convert_bitstring_to_evalue(
-        bitstring: str, link_bitmap: IrrepBitmap,
-        vertex_bitmap: VertexMultiplicityBitmap) -> float:
+        global_bitstring: str,
+        lattice_encoder: LatticeStateEncoder,
+        note_unphysical_states: bool = True,
+        stop_on_unphysical_states: bool = False) -> float:
     """
     Convert lattice links to total energy.
 
-    Used for computing the average electric energy. Currently raises a
-    NotImplemented error if vertex_bitmap is nonempty.
+    Used for computing the average electric energy. 
+    Chunks the bitstring into |v l1 l2 ..> for each vertex when 
+    there are bag states. Default behavior for unphysical states is to note
+    them and assign 0.0 energy. This can by logged with note_unphysical_states,
+    or a ValueError can be raised with stop_on_unphysical_states.
     """
-    casimirs = [_gt_pattern_iweight_to_casimir(irrep) for irrep in list(link_bitmap.keys())]
+    link_bitmap = lattice_encoder.link_bitmap
+    vertex_bitmap = lattice_encoder.vertex_bitmap
+
+    casimirs = [
+        _gt_pattern_iweight_to_casimir(irrep)
+        for irrep in list(link_bitmap.keys())
+    ]
     casimirs_dict = {}
 
-    for i, enc in enumerate(list(link_bitmap.values())):
-        casimirs_dict[enc] = casimirs[i]
+    # encode using iweights (such as (1, 0 ,0))
+    for i, iweight in enumerate(list(link_bitmap.keys())):
+        casimirs_dict[iweight] = casimirs[i]
 
     evalue = 0.0
-    len_string = len(list(link_bitmap.values())[0])
+    len_string = lattice_encoder.expected_link_bit_string_length
 
     has_vertex_bitmap_data = not len(vertex_bitmap) == 0
     if not has_vertex_bitmap_data:
-        for i in range(0, len(bitstring), len_string):
-            evalue += casimirs_dict[bitstring[i:i + len_string]]
+        for i in range(0, len(global_bitstring), len_string):
+            link_bitstring_chunk = global_bitstring[i:i + len_string]
+            link_state = lattice_encoder.decode_bit_string_to_link_state(
+                link_bitstring_chunk)
+            # Handle unphysical link state
+            if (link_state is None):
+                return _handle_electric_energy_unphysical_states(
+                    global_bitstring, link_bitstring_chunk,
+                    note_unphysical_states, stop_on_unphysical_states)
+            evalue += casimirs_dict[link_state]
     else:
-        raise NotImplementedError("Eigenvalue computation when vertex data present is a WIP.")
-        # vertex_singlet_length = len(list(vertex_bitmap.values())[0])
-        # dim = int(len(list(vertex_bitmap.keys())[0]) / 2.0)
-        # total_length = vertex_singlet_length + dim * len_string
+        vertex_singlet_length = lattice_encoder.expected_vertex_bit_string_length
+        # Find the spatial dimension of the lattice from vertex state
+        spatial_dim = int(len(list(vertex_bitmap.keys())[0][0]) / 2.0)
+        if (spatial_dim == 1):
+            total_length_x = vertex_singlet_length + (spatial_dim +
+                                                      1) * len_string
+            total_length_y = vertex_singlet_length + spatial_dim * len_string
 
-        # chunks = [
-        #     bitstring[i:i + total_length]
-        #     for i in range(0, len(bitstring, total_length))
-        # ]
+            chunks = [
+                global_bitstring[i:i + total_length_x + total_length_y]
+                for i in range(0, len(global_bitstring), total_length_x +
+                               total_length_y)
+            ]
 
-        # for chunk in chunks:
-        #     for i in range(vertex_singlet_length, len(chunk), len_string):
-        #         evalue += casimirs_dict[bitstring[i:i + len_string]]
+            for chunk in chunks:
+                x_chunk = chunk[:total_length_x]
+                y_chunk = chunk[total_length_x:total_length_x + total_length_y]
+
+                for i in range(vertex_singlet_length, total_length_x,
+                               len_string):
+                    link_bitstring_chunk = x_chunk[i:i + len_string]
+                    link_state = lattice_encoder.decode_bit_string_to_link_state(
+                        link_bitstring_chunk)
+                    # Handle unphysical link state
+                    if (link_state is None):
+                        return _handle_electric_energy_unphysical_states(
+                            global_bitstring, link_bitstring_chunk,
+                            note_unphysical_states, stop_on_unphysical_states)
+                    evalue += casimirs_dict[link_state]
+                for i in range(vertex_singlet_length, total_length_y,
+                               len_string):
+                    link_bitstring_chunk = y_chunk[i:i + len_string]
+                    link_state = lattice_encoder.decode_bit_string_to_link_state(
+                        link_bitstring_chunk)
+                    # Handle unphysical link state
+                    if (link_state is None):
+                        return _handle_electric_energy_unphysical_states(
+                            global_bitstring, link_bitstring_chunk,
+                            note_unphysical_states, stop_on_unphysical_states)
+                    evalue += casimirs_dict[link_state]
+
+        else:
+            total_length = vertex_singlet_length + spatial_dim * len_string
+
+            chunks = [
+                global_bitstring[i:i + total_length]
+                for i in range(0, len(global_bitstring), total_length)
+            ]
+
+            for chunk in chunks:
+                for i in range(vertex_singlet_length, len(chunk), len_string):
+                    link_bitstring_chunk = chunk[i:i + len_string]
+                    link_state = lattice_encoder.decode_bit_string_to_link_state(
+                        link_bitstring_chunk)
+                    if (link_state is None):
+                        return _handle_electric_energy_unphysical_states(
+                            global_bitstring, link_bitstring_chunk,
+                            note_unphysical_states, stop_on_unphysical_states)
+                    evalue += casimirs_dict[link_state]
 
     return evalue
 
@@ -99,14 +190,13 @@ def electric_hamiltonian(link_bitmap: IrrepBitmap) -> List[float]:
     """Give the total Pauli decomposition."""
     N = len(list(link_bitmap.values())[0])
     casimir_values = [
-        _gt_pattern_iweight_to_casimir(irrep) for irrep in list(link_bitmap.keys())
+        _gt_pattern_iweight_to_casimir(irrep)
+        for irrep in list(link_bitmap.keys())
     ]
     pauli_strings = [
-        _diagonal_pauli_string(
-            bit_string=list(link_bitmap.values())[i],
-            N=N,
-            casimir=casimir_values[i]
-        )
+        _diagonal_pauli_string(bit_string=list(link_bitmap.values())[i],
+                               N=N,
+                               casimir=casimir_values[i])
         for i in range(len(list(link_bitmap.values())))
     ]
 
@@ -115,7 +205,9 @@ def electric_hamiltonian(link_bitmap: IrrepBitmap) -> List[float]:
 
 # Tests
 def _test_decomp_1_3_3bar():
-    print("Testing if 1,3,3bar electric Hamiltonian is correctly formed from Pauli decomposition...")
+    print(
+        "Testing if 1,3,3bar electric Hamiltonian is correctly formed from Pauli decomposition..."
+    )
     II = np.kron([[1, 0], [0, 1]], [[1, 0], [0, 1]])
     IZ = np.kron([[1, 0], [0, 1]], [[1, 0], [0, -1]])
     ZI = np.kron([[1, 0], [0, -1]], [[1, 0], [0, 1]])
@@ -149,7 +241,9 @@ def _test_decomp_1_3_3bar():
 
 
 def _test_decomp_1_3_3bar_6_6bar_8():
-    print("Testing if 1,3,3bar,6,6bar,8 electric Hamiltonian is correctly formed from Pauli decompisition...")
+    print(
+        "Testing if 1,3,3bar,6,6bar,8 electric Hamiltonian is correctly formed from Pauli decompisition..."
+    )
     III = np.kron(np.kron([[1, 0], [0, 1]], [[1, 0], [0, 1]]),
                   [[1, 0], [0, 1]])
     IIZ = np.kron(np.kron([[1, 0], [0, 1]], [[1, 0], [0, 1]]),
@@ -203,8 +297,165 @@ def _test_decomp_1_3_3bar_6_6bar_8():
         print(decomp_matrx)
 
 
+def _test_convert_bitstring_to_evalue_case1():
+    print(
+        "Testing if the electric energy |E|^2 is being calculated correctly for d2, T1p vacuum"
+    )
+
+    # Use the T1 truncation with T1p vertex maps for ease of creating tests.
+    # Tests strings may or not physical
+    T1_map = {(0, 0, 0): "00", (1, 0, 0): "10", (1, 1, 0): "01"}
+
+    vertex_d2_T1p_map = {
+        (((0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)), 1): '0',
+        (((0, 0, 0), (0, 0, 0), (1, 0, 0), (1, 1, 0)), 1): '1',
+    }
+
+    # 20 qubits in d=2, 2x2, T1p. Expect zero energy
+    example_d2_T1p_string1 = "0" * 20
+
+    lattice_encoder = LatticeStateEncoder(T1_map, vertex_d2_T1p_map)
+
+    if (np.isclose(
+            0.0,
+            convert_bitstring_to_evalue(example_d2_T1p_string1,
+                                        lattice_encoder))):
+        print("Test passed")
+    else:
+        print("Failure for d=2, T1p vacuum. The calculated energy was " + str(
+            convert_bitstring_to_evalue(example_d2_T1p_string1,
+                                        lattice_encoder)) + " instead of " +
+              str(0.0))
+
+
+def _test_convert_bitstring_to_evalue_case2():
+    print(
+        "Testing if the electric energy |E|^2 is being calculated correctly for d2, T1p excited state"
+    )
+
+    # Use the T1 truncation with T1p vertex maps for ease of creating tests.
+    # Tests strings may or not physical
+    T1_map = {(0, 0, 0): "00", (1, 0, 0): "10", (1, 1, 0): "01"}
+
+    vertex_d2_T1p_map = {
+        (((0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)), 1): '0',
+        (((0, 0, 0), (0, 0, 0), (1, 0, 0), (1, 1, 0)), 1): '1',
+    }
+
+    # 20 qubits in d=2, 2x2, T1p. There are 6 links with energy (4/3)
+    example_d2_T1p_string2 = "10001110100101001000"
+
+    lattice_encoder = LatticeStateEncoder(T1_map, vertex_d2_T1p_map)
+
+    if (np.isclose((4.0 / 3.0) * 6.0,
+                   convert_bitstring_to_evalue(example_d2_T1p_string2,
+                                               lattice_encoder))):
+        print("Test passed")
+    else:
+        print("Failure d=2, T1p excited state. The calculated energy was " +
+              str(
+                  convert_bitstring_to_evalue(example_d2_T1p_string2,
+                                              lattice_encoder)) +
+              " instead of " + str((4.0 / 3.0) * 6.0))
+
+
+def _test_convert_bitstring_to_evalue_case3():
+    print(
+        "Testing if the electric energy |E|^2 is being calculated correctly for d3/2, T1p excited state"
+    )
+
+    # Use the T1 truncation with T1p vertex maps for ease of creating tests.
+    # Tests strings may or not physical
+    T1_map = {(0, 0, 0): "00", (1, 0, 0): "10", (1, 1, 0): "01"}
+
+    vertex_d32_T1p_map = {
+        (((0, 0, 0), (0, 0, 0), (0, 0, 0)), 1): '0',
+        (((0, 0, 0), (1, 0, 0), (1, 1, 0)), 1): '1',
+    }
+
+    # 16 qubits in d=3/2, 2x1, T1p. There are 4 links with energy (4/3)
+    example_d32_T1p_string1 = "1000111011010100"
+
+    lattice_encoder = LatticeStateEncoder(T1_map, vertex_d32_T1p_map)
+
+    if (np.isclose((4.0 / 3.0) * 4.0,
+                   convert_bitstring_to_evalue(example_d32_T1p_string1,
+                                               lattice_encoder))):
+        print("Test passed")
+    else:
+        print("Failure d=3/2, T1p excited state. The calculated energy was " +
+              str(
+                  convert_bitstring_to_evalue(example_d32_T1p_string1,
+                                              lattice_encoder)) +
+              " instead of " + str((4.0 / 3.0) * 4.0))
+
+
+def _test_convert_bitstring_to_evalue_case4():
+    print(
+        "Testing if the electric energy |E|^2 is being calculated correctly for d3/2, T1p unphysical state "
+        "with just noting an error")
+
+    # Use the T1 truncation with T1p vertex maps for ease of creating tests.
+    # Tests strings may or not physical
+    T1_map = {(0, 0, 0): "00", (1, 0, 0): "10", (1, 1, 0): "01"}
+
+    vertex_d32_T1p_map = {
+        (((0, 0, 0), (0, 0, 0), (0, 0, 0)), 1): '0',
+        (((0, 0, 0), (1, 0, 0), (1, 1, 0)), 1): '1',
+    }
+
+    # 16 qubits in d=3/2, 2x1, T1p. There is an unphysical link at ((0,0), e_y). Expecting 0.0
+    example_d32_T1p_string2 = "1001111011010100"
+
+    lattice_encoder = LatticeStateEncoder(T1_map, vertex_d32_T1p_map)
+
+    if (np.isclose(
+            0.0,
+            convert_bitstring_to_evalue(example_d32_T1p_string2,
+                                        lattice_encoder))):
+        print("Test passed")
+    else:
+        print("Failure d=3/2, T1p excited state. The calculated energy was " +
+              str(
+                  convert_bitstring_to_evalue(example_d32_T1p_string2,
+                                              lattice_encoder)) +
+              " instead of " + 0.0)
+
+
+def _test_convert_bitstring_to_evalue_case5():
+    print(
+        "Testing if the electric energy |E|^2 is being calculated correctly for d3/2, T1p unphysical state "
+        + "with raising an error")
+
+    # Use the T1 truncation with T1p vertex maps for ease of creating tests.
+    # Tests strings may or not physical
+    T1_map = {(0, 0, 0): "00", (1, 0, 0): "10", (1, 1, 0): "01"}
+
+    vertex_d32_T1p_map = {
+        (((0, 0, 0), (0, 0, 0), (0, 0, 0)), 1): '0',
+        (((0, 0, 0), (1, 0, 0), (1, 1, 0)), 1): '1',
+    }
+
+    # 16 qubits in d=3/2, 2x1, T1p. There is an unphysical link at ((0,0), e_y). Expecting 0.0
+    example_d32_T1p_string2 = "1001111011010100"
+
+    lattice_encoder = LatticeStateEncoder(T1_map, vertex_d32_T1p_map)
+
+    try:
+        convert_bitstring_to_evalue(example_d32_T1p_string2,
+                                    lattice_encoder,
+                                    stop_on_unphysical_states=True)
+    except Exception as e:
+        print("Test passed, caught exception: " + str(e))
+
+
 if __name__ == "__main__":
-    print("Running test...")
+    print("Running tests...")
     _test_decomp_1_3_3bar()
     _test_decomp_1_3_3bar_6_6bar_8()
+    _test_convert_bitstring_to_evalue_case1()
+    _test_convert_bitstring_to_evalue_case2()
+    _test_convert_bitstring_to_evalue_case3()
+    _test_convert_bitstring_to_evalue_case4()
+    _test_convert_bitstring_to_evalue_case5()
     print("All tests passed.")
